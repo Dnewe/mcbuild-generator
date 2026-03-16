8aa8c6e25a1a7214fd0c594bf271e8b7ad529fc7
Dnewe <ewenstvallier@gmail.com>
2026-03-16 16:23:18 +0100

    extraction pipeline

---
+++ src/mcbuild_generator/extraction/pipeline.py
@@ -0,0 +1,14 @@
+from mcbuild_generator.extraction.extract_metadata import extract_metadata
+from mcbuild_generator.utils.args import get_config
+
+
+def pipeline(config):
+    extract_metadata(config['data_dir'], config['metadata_csv'], config['max_files'])
+
+
+if __name__=='__main__':
+    # get args
+    config = get_config()
+
+    # run
+    pipeline(config)
