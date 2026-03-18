1826cb50fca7d8db9979b471ba0f5a5ab62bce69
Dnewe <ewenstvallier@gmail.com>
2026-03-18 19:04:04 +0100

    updated training (cuda amp) + kaggle training notebook

--- src/mcbuild_generator/training/vae/train.py
+++ src/mcbuild_generator/training/vae/train.py
@@ -1,5 +1,4 @@
 import torch.optim as optim
-import torch.nn as nn
 import torch
 from tqdm import tqdm
 
@@ -7,9 +6,11 @@ from mcbuild_generator.training.vae.vae import VAE
 from mcbuild_generator.training.vae.vae_loss import vae_loss
 
 
-def train(model:VAE, train_loader, val_loader, epochs, lr, kl_weight, device):
+def train(model: VAE, train_loader, val_loader, epochs, lr, kl_weight, device):
     model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
+    use_amp = torch.cuda.is_available()
+    scaler = torch.GradScaler(enabled=use_amp)
 
     train_losses = []
     val_losses = []
@@ -19,20 +20,36 @@ def train(model:VAE, train_loader, val_loader, epochs, lr, kl_weight, device):
         tqdm_loader = tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}")
 
         for x_list in tqdm_loader:
+            optimizer.zero_grad(set_to_none=True)
             x_list = [x.to(device) for x in x_list]
-            recon_x_list, mu_list, logvar_list = model(x_list)
 
-            loss, ce_loss, kl_loss = vae_loss(recon_x_list, x_list, mu_list, logvar_list, kl_weight)
+            with torch.autocast("cuda", enabled=use_amp):
+                recon_x_list, mu_list, logvar_list = model(x_list)
+                loss, ce_loss, kl_loss = vae_loss(
+                    recon_x_list, x_list, mu_list, logvar_list, kl_weight
+                )
 
-            optimizer.zero_grad()
-            loss.backward()  # type: ignore
-            optimizer.step()
+            scaler.scale(loss).backward()  # type: ignore
+            scaler.step(optimizer)
+            scaler.update()
+            # loss.backward() # type: ignore
+            # optimizer.step()
 
             total_loss += loss.item() # type: ignore
-            tqdm_loader.set_postfix({'loss':loss.item(), 'ce_loss': ce_loss.item(), 'kl_loss': kl_loss.item()}) # type: ignore
+            tqdm_loader.set_postfix(
+                {
+                    "loss": loss.item(), # type: ignore
+                    "ce_loss": ce_loss.item(), # type: ignore
+                    "kl_loss": kl_loss.item(), # type: ignore
+                }
+            ) 
 
         avg_train_loss = total_loss / len(train_loader.dataset)
-        avg_val_loss = evaluate_model(model, val_loader, kl_weight, device)
+        avg_val_loss = evaluate_model(model, val_loader, kl_weight, device, use_amp)
+
+        print(
+            f"[{epoch}/{epochs}]: train_loss= {avg_train_loss:.3f} | val_loss= {avg_val_loss:.3f}"
+        )
 
         train_losses.append(avg_train_loss)
         val_losses.append(avg_val_loss)
@@ -40,17 +57,20 @@ def train(model:VAE, train_loader, val_loader, epochs, lr, kl_weight, device):
     return train_losses, val_losses
 
 
-def evaluate_model(model, loader, kl_weight, device):
+def evaluate_model(model, loader, kl_weight, device, use_amp):
     model.eval()
     total_loss = 0
-    loss_fn = nn.CrossEntropyLoss()
     with torch.no_grad():  # No need to compute gradients
-        for x in tqdm(loader, desc="Evaluating"):
+        for x_list in tqdm(loader, desc="Evaluating"):
             x_list = [x.to(device) for x in x_list]
-            recon_x_list, mu_list, logvar_list = model(x_list)
 
-            loss, ce_loss, kl_loss = vae_loss(recon_x_list, x_list, mu_list, logvar_list, kl_weight)
-            total_loss += loss.item() # type: ignore
+            with torch.autocast("cuda", enabled=use_amp):
+                recon_x_list, mu_list, logvar_list = model(x_list)
+                loss, _, _ = vae_loss(
+                    recon_x_list, x_list, mu_list, logvar_list, kl_weight
+                )
+
+            total_loss += loss.item()  # type: ignore
 
     avg_loss = total_loss / len(loader.dataset)
     return avg_loss
